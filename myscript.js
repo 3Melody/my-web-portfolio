318a30653f24cb8172bd6dd010a328aa0e752595
3Melody <plugeiei2003@gmail.com>
2023-08-23 23:17:52 +0700

    day1
    moblie-res

---
+++ myscript.js
@@ -0,0 +1,21 @@
+let sections = document.querySelectorAll('section');
+const sec1 = document.getElementById('sec1');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if(top >= offset && top < offset + height){
+            sec.classList.add('show-animate');
+        }
+        else{
+            sec.classList.remove('show-animate');
+        }
+    })
+};
+
+window.addEventListener("load",function(){
+    sec1.classList.add('show-animate');
+})
