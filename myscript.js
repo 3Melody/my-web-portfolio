8eddaa0c4e57954010046131e08840786f549db1
3Melody <plugeiei2003@gmail.com>
2023-08-24 22:01:15 +0700

    day 2

--- myscript.js
+++ myscript.js
@@ -6,7 +6,7 @@ window.onscroll = () =>{
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-
+        
         if(top >= offset && top < offset + height){
             sec.classList.add('show-animate');
         }
